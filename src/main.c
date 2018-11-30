db8fcb54a6552d6f8ea01b74cdf44af3ad8b8f03
Tyler <5952757+TylerHann@users.noreply.github.com>
2018-11-30 13:32:20 -0600

    Formatting

--- src/main.c
+++ src/main.c
@@ -436,7 +436,7 @@ __attribute__((section(".boot"))) int main(void) {
 
 					// init the public key display to "no public key".
 					display_no_public_key();
-                    
+
 					// set menu bar colour for blue
 					ui_set_menu_bar_colour();
 
